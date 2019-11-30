2fe8bb6ba20809060e5d8ec346f3b27eeed7e6fc
junhyeog <junhyeog@korea.ac.kr>
2019-11-30 12:14:55 +0900

    last

--- pages/_app.js
+++ pages/_app.js
@@ -1,21 +1,18 @@
 import React from 'react';
-import { Container } from 'next/app';
 import Head from 'next/head';
 import { createGlobalStyle } from 'styled-components'
 
 
 const Layout = ({ Component }) => {
-    return (
-        <Container>
-            <Head>
-                <title>My Workstation</title>
-                <link rel="icon" type="image/x-icon" href="static/titleIcon.ico" />
-                <link href="https://fonts.googleapis.com/css?family=Noto+Sans+KR&display=swap" rel="stylesheet" />
-                <link href="https://fonts.googleapis.com/css?family=Dynalight" rel="stylesheet" />
-            </Head>
-            <Component />
-            <GlobalStyle />
-        </Container>);
+  return (
+    <>
+      <Head>
+        <title>My Workstation</title>
+        <link rel="icon" type="image/x-icon" href="static/titleIcon.ico" />
+      </Head>
+      <Component />
+      <GlobalStyle />
+    </>);
 }
 
 export default Layout;
